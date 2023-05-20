55dd970fbbbf451f03af96328903d98c10c11491
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-20 21:44:06 +0100

    fixed connection bugs

--- app.js
+++ app.js
@@ -4,17 +4,15 @@ const express = require('express')
 const expressLayout = require('express-ejs-layouts');
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = 4000 || process.env.PORT;
 
-app.use(express.statis('public'));
+app.use(express.static('public'));
 
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs')
 
-app.get('', (req, res) => {
-    res.send("Hello world");
-});
+app.use('/', require('./server/routes/main.js'))
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
